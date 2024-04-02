3b43af2716cc1dbb3597d94a238fedc369c484cb
Abdel <abdelhakmerabtene@gmail.com>
2024-04-02 12:54:21 +0200

    Correct one I hope

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     accessKeyId: 'NX11XRFDHC55HI9YFU81', // TODO
     secretAccessKey: 'dS9mU72hEpmQSW2UHNs3PQB5FnjUWs3f1fJl4Son', // TODO
     endpoint: 's3.eu-west-2.wasabisys.com', // TODO
-    bucket: 'eesgi-cloud-exam-bucket-60291', // TODO
+    bucket: 'esgi-cloud-exam-bucket-60291', // TODO
   },
   suffix: {
     small: '_small.jpg',
