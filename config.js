6f9350b6b9d83e506c3fc91da782d9e22a93f036
Abdel <abdelhakmerabtene@gmail.com>
2024-04-02 12:21:30 +0200

    Updated config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: 'NX11XRFDHC55HI9YFU81', // TODO
+    secretAccessKey: 'dS9mU72hEpmQSW2UHNs3PQB5FnjUWs3f1fJl4Son', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi2', // TODO
   },
   suffix: {
     small: '_small.jpg',
